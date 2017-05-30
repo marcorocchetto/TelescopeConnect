470863fe797f07a81d2185e0d7dd938085a0b79f
Marco Rocchetto <rocchetto.marco@gmail.com>
2017-05-30 09:04:59 +0800

    adding download.py script

---
+++ download.py
@@ -0,0 +1,68 @@
+'''
+
+Download frames
+
+The input to the script is a json file with the following format. See input_examples
+
+Example:
+
+python download.py --json=input_examples/download_files.json
+
+'''
+
+import argparse
+from astropy.io import fits
+import sys
+import numpy as np
+from time import strftime
+import os
+import zipfile, random, string
+import shutil
+
+sys.path.append('./library')
+
+import general
+from general import *
+
+#loading parameter file parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--json',
+                    dest='json_filename',
+                    type=str,
+                    default=False,
+                    )
+
+options = parser.parse_args()
+
+if not options.json_filename:
+    RaiseError('You need an inpunt JSON file')
+
+json_data = json.loads(open(options.json_filename).read())
+
+# read all files from list
+# images = []
+# for image_fname in json_data['input_fits']:
+#     images.append(get_ImageData(image_fname))
+
+# TEMPORARY. THIS DOES NOT DO ANYTHING, JUST COMPRESS ALL FILES INTO SINGLE ZIP
+random_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+
+# create directory for files to be compressed
+zip_directory = os.path.join(json_data['output_folder'], random_string)
+if not os.path.exists(zip_directory):
+    os.makedirs(zip_directory)
+
+for image_fname in json_data['input_fits']:
+    shutil.copy(image_fname, zip_directory)
+
+output_filename = random_string + '.zip'
+
+shutil.make_archive(os.path.join(json_data['output_folder'], random_string), 'zip', zip_directory)
+
+shutil.rmtree(zip_directory)
+
+print(json.dumps({'result': 'SUCCESS',
+                  'output_file': output_filename,
+                  'output_folder': json_data['output_folder']},
+                 separators=(',', ':'), indent=4))
+sys.exit()
