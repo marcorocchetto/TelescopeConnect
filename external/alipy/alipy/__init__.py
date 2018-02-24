b64f9e4838cf78da4d42e3850eceae887ab6d0d4
Marco Rocchetto <rocchetto.marco@gmail.com>
2018-02-24 14:01:01 +0000

    include alipy

---
+++ external/alipy/alipy/__init__.py
@@ -0,0 +1,21 @@
+"""
+This is alipy, a python package to quickly, automatically, and robustly identify geometrical transforms between optical astronomical images.
+
+:Authors: Malte Tewes
+:License: GPLv3
+
+
+"""
+
+__author__ = "Malte Tewes"
+__copyright__ = "2012, Malte Tewes"
+__version__ = "2.0"
+
+from . import imgcat
+from . import pysex
+from . import star
+from . import quad
+from . import ident
+from . import align
+
+
