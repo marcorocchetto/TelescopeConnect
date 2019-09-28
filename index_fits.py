6368a2b0bb2a0b121ec96a540bec24370e431398
Marco Rocchetto <rocchetto.marco@gmail.com>
2019-09-28 21:38:54 +0100

    quick fix report errors

--- index_fits.py
+++ index_fits.py
@@ -45,233 +45,255 @@ parser.add_argument('--json',
 
 options = parser.parse_args()
 
-if not options.json_filename:
-    RaiseError('You need an inpunt JSON file')
+def return_error(error):
 
-json_data = json.loads(open(options.json_filename).read())
+    output = {
+        'result': 'SUCCESS',
+        'message': error
+    }
+
+    print(json.dumps(output, separators=(',',':'), sort_keys=True, indent=4))
 
 
-if not json_data['fits_fname']:
-    RaiseError('File name not specified')
-
-if not json_data['time_zone']:
-    local = pytz.timezone('Etc/GMT')
-else:
-    try:
-        local = pytz.timezone(json_data['time_zone'])
-    except pytz.exceptions.UnknownTimeZoneError:
-        local = pytz.timezone('Etc/GMT')
-
-# check file exists
-if not os.path.isfile(json_data['fits_fname']):
-    RaiseError('File not found')
-
-# open fits file hdu
 try:
-    hdulist = fits.open(json_data['fits_fname'], ignore_missing_end=True)
-except:
-    RaiseError('Unexpected error:', sys.exc_info()[0])
 
-# get header & data
-header = hdulist[0].header
-data = hdulist[0].data
+    if not options.json_filename:
+        RaiseError('You need an inpunt JSON file')
 
-# temporary fix for PinPoint simulated images
-if not 'IMAGETYP' in hdulist[0].header:
-    header['IMAGETYP'] = 'Light'
-
-# check that some required FITS headers are present, otherwise raise error
-#header_keys = ['DATE-OBS', 'IMAGETYP', 'CCD-TEMP', 'EXPTIME']
-header_keys = ['DATE-OBS', 'CCD-TEMP', 'EXPTIME'] # temp fix
-for key in header_keys:
-    if not key in hdulist[0].header:
-        RaiseError('Header key `%s` missing' % key)
+    json_data = json.loads(open(options.json_filename).read())
 
 
-# determine Image type from header IMAGETYP
-imagetype = get_ImageType(header['IMAGETYP'])
-if not imagetype:
-    RaiseError('Cannot determine ImageType')
+    if not json_data['fits_fname']:
+        RaiseError('File name not specified')
 
-# Date and time of observation: keyword DATE-OBS
-dateobs_utc_str, dateobs_utc_datetime = get_DateObs(header['DATE-OBS'])
+    if not json_data['time_zone']:
+        local = pytz.timezone('Etc/GMT')
+    else:
+        try:
+            local = pytz.timezone(json_data['time_zone'])
+        except pytz.exceptions.UnknownTimeZoneError:
+            local = pytz.timezone('Etc/GMT')
+
+    # check file exists
+    if not os.path.isfile(json_data['fits_fname']):
+        RaiseError('File not found')
+
+    # open fits file hdu
+    try:
+        hdulist = fits.open(json_data['fits_fname'], ignore_missing_end=True)
+    except:
+        RaiseError('Unexpected error:', sys.exc_info()[0])
+
+    # get header & data
+    header = hdulist[0].header
+
+    try:
+        data = hdulist[0].data
+    except:
+        RaiseError('Unexpected error:', sys.exc_info()[0])
+
+    # temporary fix for PinPoint simulated images
+    if not 'IMAGETYP' in hdulist[0].header:
+        header['IMAGETYP'] = 'Light'
+
+    # check that some required FITS headers are present, otherwise raise error
+    #header_keys = ['DATE-OBS', 'IMAGETYP', 'CCD-TEMP', 'EXPTIME']
+    header_keys = ['DATE-OBS', 'CCD-TEMP', 'EXPTIME'] # temp fix
+    for key in header_keys:
+        if not key in hdulist[0].header:
+            RaiseError('Header key `%s` missing' % key)
 
 
-# Determine OBSNIGHT, i.e. night of observation
-obsnight_str = get_ObsNight(dateobs_utc_datetime, local)
+    # determine Image type from header IMAGETYP
+    imagetype = get_ImageType(header['IMAGETYP'])
+    if not imagetype:
+        RaiseError('Cannot determine ImageType')
+
+    # Date and time of observation: keyword DATE-OBS
+    dateobs_utc_str, dateobs_utc_datetime = get_DateObs(header['DATE-OBS'])
 
 
-# RA and DEC, header keys: OBJCTRA, OBJCTDEC or RA, DEC
-if 'OBJCTRA' in header:
-    ra_str = header['OBJCTRA']
-    ra = SexToDeg(ra_str, 'ra')
-elif 'RA' in header:
-    ra_str = header['RA']
-    ra = SexToDeg(ra_str, 'ra')
-else:
-    ra_str = ''
-    ra = None
-if 'OBJCTDEC' in header:
-    dec_str = header['OBJCTDEC']
-    dec = SexToDeg(dec_str, 'dec')
-elif 'DEC' in header:
-    dec_str = header['OBJCTDEC']
-    dec = SexToDeg(dec_str, 'dec')
-else:
-    dec_str = ''
-    dec = None
+    # Determine OBSNIGHT, i.e. night of observation
+    obsnight_str = get_ObsNight(dateobs_utc_datetime, local)
 
-# optional headers: TELESCOP, INSTRUME, OBSERVER, OBJECT
-if 'TELESCOP' in header:
-    telescope = header['TELESCOP']
-else:
-    telescope = ''
-if 'INSTRUME' in header:
-    instrument = header['INSTRUME']
-else:
-    instrument = ''
-if 'OBSERVER' in header:
-    observer = header['OBSERVER']
-else:
-    observer = ''
-if 'OBJECT' in header:
-    object = header['OBJECT']
-else:
-    object = ''
 
-# exposure time
-exptime = header['EXPTIME']
+    # RA and DEC, header keys: OBJCTRA, OBJCTDEC or RA, DEC
+    if 'OBJCTRA' in header:
+        ra_str = header['OBJCTRA']
+        ra = SexToDeg(ra_str, 'ra')
+    elif 'RA' in header:
+        ra_str = header['RA']
+        ra = SexToDeg(ra_str, 'ra')
+    else:
+        ra_str = ''
+        ra = None
+    if 'OBJCTDEC' in header:
+        dec_str = header['OBJCTDEC']
+        dec = SexToDeg(dec_str, 'dec')
+    elif 'DEC' in header:
+        dec_str = header['OBJCTDEC']
+        dec = SexToDeg(dec_str, 'dec')
+    else:
+        dec_str = ''
+        dec = None
 
-# filter
-if not 'FILTER' in header:
-    header['FILTER'] = 'Clear'
-
-if 'FILTER' in header and imagetype != 'BIAS':
-    filter = header['FILTER']
-else:
-    filter = ''
-
-# ccd temperature
-if not 'CCD-TEMP' in header:
-    ccdtemp = 0
-else:
-    ccdtemp = header['CCD-TEMP']
-
-# image size
-width_px = len(data[0,:])
-height_px = len(data[:,0])
-
-# set filename
-filename = dateobs_utc_datetime.strftime('%Y-%m-%dT%H-%M-%S')
-if imagetype == 'LIGHT':
+    # optional headers: TELESCOP, INSTRUME, OBSERVER, OBJECT
+    if 'TELESCOP' in header:
+        telescope = header['TELESCOP']
+    else:
+        telescope = ''
+    if 'INSTRUME' in header:
+        instrument = header['INSTRUME']
+    else:
+        instrument = ''
+    if 'OBSERVER' in header:
+        observer = header['OBSERVER']
+    else:
+        observer = ''
     if 'OBJECT' in header:
-        filename += '-' + header['OBJECT']
-    filename += '-' + header['FILTER'].replace("'", "prime")
-if imagetype == 'BIAS':
-    filename += '-Bias'
-if imagetype == 'DARK':
-    filename += '-Dark'
-if imagetype == 'FLAT':
-    filename += '-Flat-'
-    filename += header['FILTER'].replace("'", "prime")
-filename = filename.replace(' ', '_')
+        object = header['OBJECT']
+    else:
+        object = ''
 
-filename_fits = filename + '.fits'
+    # exposure time
+    exptime = header['EXPTIME']
 
-filename_jpg_large = filename + '.jpg'
-filename_jpg_thumb = filename + '_thumb.jpg'
+    # filter
+    if not 'FILTER' in header:
+        header['FILTER'] = 'Clear'
 
-path_jpg_large = os.path.abspath(os.path.join(json_data['output_folder'], filename_jpg_large))
-path_jpg_thumb = os.path.abspath(os.path.join(json_data['output_folder'], filename_jpg_thumb))
+    if 'FILTER' in header and imagetype != 'BIAS':
+        filter = header['FILTER']
+    else:
+        filter = ''
 
-os.system("/usr/bin/convert '" + os.path.abspath(json_data['fits_fname']) + "' -contrast-stretch 3%  -resize 1024x '" + path_jpg_large + "'")
-os.system("/usr/bin/convert '" + os.path.abspath(json_data['fits_fname']) + "' -contrast-stretch 3%  -resize 100x '" + path_jpg_thumb + "'")
+    # ccd temperature
+    if not 'CCD-TEMP' in header:
+        ccdtemp = 0
+    else:
+        ccdtemp = header['CCD-TEMP']
 
-# get seeing
-if not 'pixel_scale' in json_data:
-    seeing = 0
-else:
+    # image size
+    width_px = len(data[0,:])
+    height_px = len(data[:,0])
 
-    PixelScale = json_data['pixel_scale']
+    # set filename
+    filename = dateobs_utc_datetime.strftime('%Y-%m-%dT%H-%M-%S')
+    if imagetype == 'LIGHT':
+        if 'OBJECT' in header:
+            filename += '-' + header['OBJECT']
+        filename += '-' + header['FILTER'].replace("'", "prime")
+    if imagetype == 'BIAS':
+        filename += '-Bias'
+    if imagetype == 'DARK':
+        filename += '-Dark'
+    if imagetype == 'FLAT':
+        filename += '-Flat-'
+        filename += header['FILTER'].replace("'", "prime")
+    filename = filename.replace(' ', '_')
 
-    with open(os.devnull, 'w') as devnull:
-        with contextlib.redirect_stdout(devnull):
-            out = sew(json_data['fits_fname'])
-            seeing = round(np.median(out["table"][0][:]) * PixelScale, 1)
+    filename_fits = filename + '.fits'
 
-# clean fits headers
-if 'WXSENSOR' in header:
-    del header['WXSENSOR']
-if 'SWSERIAL' in header:
-    del header['SWSERIAL']
-if 'HISTORY' in header:
-    del header['HISTORY']
-if 'COMMENT' in header:
-    del header['COMMENT']
-if 'PRESSURE' in header:
-    del header['PRESSURE']
-if 'SBSTDVER' in header:
-    del header['SBSTDVER']
-if 'SWOWNER' in header:
-    del header['SWOWNER']
-if 'PLTSOLVD' in header:
-    del header['PLTSOLVD']
+    filename_jpg_large = filename + '.jpg'
+    filename_jpg_thumb = filename + '_thumb.jpg'
 
-if 'telescope_model' in json_data:
-    header['TELESCOP'] = json_data['telescope_model']
+    path_jpg_large = os.path.abspath(os.path.join(json_data['output_folder'], filename_jpg_large))
+    path_jpg_thumb = os.path.abspath(os.path.join(json_data['output_folder'], filename_jpg_thumb))
 
-if 'imager_name' in json_data:
-    header['INSTRUME'] = json_data['imager_name']
+    os.system("/usr/bin/convert '" + os.path.abspath(json_data['fits_fname']) + "' -contrast-stretch 3%  -resize 1024x '" + path_jpg_large + "'")
+    os.system("/usr/bin/convert '" + os.path.abspath(json_data['fits_fname']) + "' -contrast-stretch 3%  -resize 100x '" + path_jpg_thumb + "'")
 
-header['OBSERVER'] = 'TelescopeLive'
+    # get seeing
+    if not 'pixel_scale' in json_data:
+        seeing = 0
+    else:
 
-if 'telescope_guid' in json_data:
-    header['TELID'] = json_data['telescope_guid']
+        PixelScale = json_data['pixel_scale']
 
-if 'imager_guid' in json_data:
-    header['IMAGERID'] = json_data['imager_guid']
+        with open(os.devnull, 'w') as devnull:
+            with contextlib.redirect_stdout(devnull):
+                out = sew(json_data['fits_fname'])
+                seeing = round(np.median(out["table"][0][:]) * PixelScale, 1)
 
-if 'frame_guid' in json_data:
-    header['FRAMEID'] = json_data['frame_guid']
+    # clean fits headers
+    if 'WXSENSOR' in header:
+        del header['WXSENSOR']
+    if 'SWSERIAL' in header:
+        del header['SWSERIAL']
+    if 'HISTORY' in header:
+        del header['HISTORY']
+    if 'COMMENT' in header:
+        del header['COMMENT']
+    if 'PRESSURE' in header:
+        del header['PRESSURE']
+    if 'SBSTDVER' in header:
+        del header['SBSTDVER']
+    if 'SWOWNER' in header:
+        del header['SWOWNER']
+    if 'PLTSOLVD' in header:
+        del header['PLTSOLVD']
 
-# save modified fits
-hdulist[0].header = header
-hdulist.writeto(json_data['fits_fname'], overwrite=True)
+    if 'telescope_model' in json_data:
+        header['TELESCOP'] = json_data['telescope_model']
 
-output = {
-    'result': 'SUCCESS',
-    'dateobs_utc_str': dateobs_utc_str,
-    'obsnight_str': obsnight_str,
-    'imagetype': imagetype,
-    'object': object,
-    'ra_float': ra,
-    'dec_float': dec,
-    'ra_str': ra_str,
-    'dec_str': dec_str,
-    'telescope': telescope,
-    'instrument': instrument,
-    'observer': observer,
-    'exptime': exptime,
-    'filter': filter,
-    'width_px': width_px,
-    'height_px': height_px,
-    'ccdtemp': ccdtemp,
-    'indexing_version': get_Version(),
-    'filename': filename,
-    'jpg_large': path_jpg_large,
-    'jpg_thumb': path_jpg_thumb,
-    'qa_passed': False,
-    'poor_fwhm': False,
-    'background_gradient': False,
-    'no_stars': False,
-    'ellipticity': 0,
-    'strikes': False,
-    'star_number': 0,
-    'seeing': seeing,
-    'internal_reflection': False,
-    'flat_field_residuals': False,
-    'rbi_residuals': False,
-    'saturated ': False
-}
+    if 'imager_name' in json_data:
+        header['INSTRUME'] = json_data['imager_name']
 
-print(json.dumps(output, separators=(',',':'), sort_keys=True, indent=4))
+    header['OBSERVER'] = 'TelescopeLive'
+
+    if 'telescope_guid' in json_data:
+        header['TELID'] = json_data['telescope_guid']
+
+    if 'imager_guid' in json_data:
+        header['IMAGERID'] = json_data['imager_guid']
+
+    if 'frame_guid' in json_data:
+        header['FRAMEID'] = json_data['frame_guid']
+
+    # save modified fits
+    hdulist[0].header = header
+    hdulist.writeto(json_data['fits_fname'], overwrite=True)
+
+    output = {
+        'result': 'SUCCESS',
+        'dateobs_utc_str': dateobs_utc_str,
+        'obsnight_str': obsnight_str,
+        'imagetype': imagetype,
+        'object': object,
+        'ra_float': ra,
+        'dec_float': dec,
+        'ra_str': ra_str,
+        'dec_str': dec_str,
+        'telescope': telescope,
+        'instrument': instrument,
+        'observer': observer,
+        'exptime': exptime,
+        'filter': filter,
+        'width_px': width_px,
+        'height_px': height_px,
+        'ccdtemp': ccdtemp,
+        'indexing_version': get_Version(),
+        'filename': filename,
+        'jpg_large': path_jpg_large,
+        'jpg_thumb': path_jpg_thumb,
+        'qa_passed': False,
+        'qa_pointing': False,
+        'qa_seeing': False,
+        'poor_fwhm': False,
+        'background_gradient': False,
+        'no_stars': False,
+        'ellipticity': 0,
+        'strikes': False,
+        'star_number': 0,
+        'seeing': seeing,
+        'internal_reflection': False,
+        'flat_field_residuals': False,
+        'rbi_residuals': False,
+        'saturated ': False
+    }
+
+    print(json.dumps(output, separators=(',',':'), sort_keys=True, indent=4))
+
+except:
+    
+    RaiseError('Unexpected error:', sys.exc_info()[0])
